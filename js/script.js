70e67167f58e7801d83829d8c82d1b04e37e5241
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-04-09 12:59:56 +0200

    add find the correct article js/script.js

--- js/script.js
+++ js/script.js
@@ -40,7 +40,8 @@ const titleClickHandler = function (event) {
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
-
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
 
     /* add class 'active' to the correct article */
