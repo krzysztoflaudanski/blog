d9b78d038b1ba79ff45e74fa7f80002563301f39
Krzysztof Laudanski <laudanski.krzysztof86@gmail.com>
2023-04-09 12:46:36 +0200

    add get href atribute js/script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 // });
 
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -28,17 +29,24 @@ const titleClickHandler = function (event) {
 
     const activeArticles = document.querySelectorAll('.posts article.active');
 
-    for (let activeArticle of activeArticles){
+    for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
-    /* add class 'active' to the correct article */
-}
 
+
+
+    /* add class 'active' to the correct article */
+
+
+}
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
